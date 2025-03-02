e8f9d5fa0e58b93208f2206b77960eae1af41bbf
Nikitty1226 <mn_snow1226@yahoo.co.jp>
2025-03-02 22:32:00 +0900

    ハートビートとドアセンサー追加

--- Doorsensor_AWS.py
+++ Doorsensor_AWS.py
@@ -1,28 +1,46 @@
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import RPi.GPIO as GPIO
+
+import threading
 import json
 import os
 import logging
 import time
-from datetime import datetime, timedelta
+from datetime import datetime
 from dotenv import load_dotenv
 
 
 # 環境変数読み込み
 load_dotenv()
 
-client = os.getenv("CLIENT")
-endpoint_url = os.getenv("ENDPOINT_URL")
-root_ca_path = os.getenv("AWS_ROOT_CA_PATH")
-private_key_path = os.getenv("AWS_PRIVATE_KEY_PATH")
-certificate_path = os.getenv("AWS_CERTIFICATE_PATH")
-topic = os.getenv("TOPIC")
-interval = os.getenv("INTERVAL")
+env_keys = [
+    "CLIENT",
+    "ENDPOINT_URL",
+    "AWS_ROOT_CA_PATH",
+    "AWS_PRIVATE_KEY_PATH",
+    "AWS_CERTIFICATE_PATH",
+    "SENSOR_TOPIC",
+    "HEARTBEAT_TOPIC",
+    "INTERVAL",
+    "GPIO_PIN"
+]
 
-if interval:
-    interval = float(interval)
+env_values = {key: os.getenv(key) for key in env_keys}
 
-if not all([client, endpoint_url, root_ca_path, private_key_path, certificate_path, topic, interval]):
-    print("環境変数が設定されていません")
+missing_keys = [key for key in env_keys if env_values[key] is None]
+if missing_keys:
+    logging.error(f"環境変数に未設定の項目があります:{missing_keys}")
+    exit(1)
+
+client, endpoint_url, root_ca_path, private_key_path, certificate_path, sensor_topic, heartbeat_topic, interval, gpio_pin = (
+    env_values.values()
+)
+
+try:
+    interval = int(interval)
+    gpio_pin = int(gpio_pin)
+except:
+    logging.error("環境変数 INTERVAL・GPIO_PIN の値が無効です")
     exit(1)
 
 # ログ設定
@@ -32,7 +50,7 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
 
-logging.getLogger('AWSIoTPythonSDK').setLevel(logging.WARNING)  # より少ないログを表示
+logging.getLogger('AWSIoTPythonSDK').setLevel(logging.WARNING)
 
 # 初期化
 myMQTTClient = AWSIoTMQTTClient(client)
@@ -45,6 +63,15 @@ myMQTTClient.configureDrainingFrequency(2) #再接続後にためていたメッ
 myMQTTClient.configureConnectDisconnectTimeout(10) #AWSIoTCoreへの接続・切断の時間制限(秒)を設定
 myMQTTClient.configureMQTTOperationTimeout(5) #メッセージ送信・受信（QoS1の場合）にかかる時間の制限を設定
 
+# GPIOの設定
+try:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(gpio_pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+except Exception as e:
+    logging.error(f"GPIO設定エラー: {e}")
+    exit(1)
+
+# プログラムの開始通知
 logging.info("通知：プログラム開始")
 
 def connect_to_aws():
@@ -61,27 +88,70 @@ def connect_to_aws():
 
 def publish_message():
     """AWSIoTCoreへメッセージを送信"""
+
+    # GPIOと時刻の初期状態の入力
+    past_value = GPIO.input(gpio_pin)
+
     while True:
+        value = GPIO.input(gpio_pin)
+        if value != past_value:
+            if value == 1: #扉が開いたとき
+
+                nowtime = datetime.now()
+
+                msgjson = {
+                    "timestamp": f"{nowtime}",
+                    "sensor_value": value
+                }
+
+                try:
+                    myMQTTClient.publish(sensor_topic, json.dumps(msgjson), 1)
+                    logging.info(f"センサデータ送信成功通知：{msgjson}")
+
+                except Exception as e:
+                    logging.error(f"AWSパブリッシュエラー通知：{e}")
+                    logging.info("通知：AWSとの再接続を試みます")
+                    connect_to_aws()
+                
+        past_value = value
+        time.sleep(1)
+
+def send_heartbeat():
+    """一定時間ごとに死活監視のメッセージを送信"""
+
+    while True:
+
+        nowtime = datetime.now()
+
         msgjson = {
-            "test1": 1,
-            "test2": 2
+            "timestamp": f"{nowtime}",
+            "status": 1
         }
 
         try:
-            myMQTTClient.publish(topic, json.dumps(msgjson), 1)
-            logging.info(f"AWSパブリッシュ成功通知：{msgjson}")
+            myMQTTClient.publish(heartbeat_topic, json.dumps(msgjson), 1)
+            logging.info(f"ハートビート送信成功通知: {msgjson}")
 
         except Exception as e:
-            logging.error(f"AWSパブリッシュエラー通知：{e}")
+            logging.error(f"ハートビート送信失敗通知: {e}")
+            logging.info("通知：AWSとの再接続を試みます")
             connect_to_aws()
-
+        
         time.sleep(interval)
 
 def main():
     """メイン処理"""
     try:
         connect_to_aws()
-        publish_message()
+
+        event_thread = threading.Thread(target=publish_message, daemon=True)
+        heartbeat_thread = threading.Thread(target=send_heartbeat, daemon=True)
+
+        event_thread.start()
+        heartbeat_thread.start()
+
+        event_thread.join()
+        heartbeat_thread.join()        
 
     except KeyboardInterrupt:
         logging.info("通知：手動停止（Control+C）を検知")
@@ -92,6 +162,7 @@ def main():
     finally:
         logging.info("通知：プログラム終了")
         myMQTTClient.disconnect()
+        GPIO.cleanup()
         exit(1)
 
 if __name__ == "__main__":
