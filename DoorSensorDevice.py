8b8ab02ed3472865caad5557e32d1fb1f62cc772
nikimasayoshi <mn_snow1226@yahoo.co.jp>
2025-06-15 18:59:49 +0900

    DoorSensorDevice.pyの作成

--- DoorSensorDevice.py
+++ DoorSensorDevice.py
@@ -6,22 +6,26 @@ import json
 import os
 import logging
 import time
-from datetime import datetime, timedelta
+from datetime import datetime, timezone, timedelta
 from dotenv import load_dotenv
 
 
 # 環境変数読み込み
 load_dotenv()
 
-client = os.getenv("CLIENT")
-endpoint_url = os.getenv("ENDPOINT_URL")
-root_ca_path = os.getenv("AWS_ROOT_CA_PATH")
-private_key_path = os.getenv("AWS_PRIVATE_KEY_PATH")
-certificate_path = os.getenv("AWS_CERTIFICATE_PATH")
-sensor_topic = os.getenv("SENSOR_TOPIC")
-interval = int(os.getenv("INTERVAL"))
-reconnection_interval = int(os.getenv("RECONNECTION_INTERVAL"))
-gpio_pin = int(os.getenv("GPIO_PIN"))
+try:
+    client = os.getenv("CLIENT")
+    endpoint_url = os.getenv("ENDPOINT_URL")
+    root_ca_path = os.getenv("AWS_ROOT_CA_PATH")
+    private_key_path = os.getenv("AWS_PRIVATE_KEY_PATH")
+    certificate_path = os.getenv("AWS_CERTIFICATE_PATH")
+    sensor_topic = os.getenv("SENSOR_TOPIC")
+    publish_interval = int(os.getenv("PUBLISH_INTERVAL"))
+    reconnection_interval = int(os.getenv("RECONNECTION_INTERVAL"))
+    gpio_pin = int(os.getenv("GPIO_PIN"))
+except Exception as e:
+    logging.error(f"環境変数設定エラー: {e}")
+    exit(1)   
 
 # ログ設定
 logging.basicConfig(
@@ -29,19 +33,12 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
+logging.getLogger('AWSIoTPythonSDK').setLevel(logging.ERROR)
 
-logging.getLogger('AWSIoTPythonSDK').setLevel(logging.CRITICAL)
-
-# 初期化
+# MQTTClient初期化
 myMQTTClient = AWSIoTMQTTClient(client)
-
-# MQTTクライアントの設定
 myMQTTClient.configureEndpoint(endpoint_url, 443)
 myMQTTClient.configureCredentials(root_ca_path, private_key_path, certificate_path)
-myMQTTClient.configureOfflinePublishQueueing(-1) #オフライン中に送信できなかったメッセージをキューにためる
-myMQTTClient.configureDrainingFrequency(2) #再接続後にためていたメッセージを1秒間に何回送信するか
-myMQTTClient.configureConnectDisconnectTimeout(10) #AWSIoTCoreへの接続・切断の時間制限(秒)を設定
-myMQTTClient.configureMQTTOperationTimeout(5) #メッセージ送信・受信（QoS1の場合）にかかる時間の制限を設定
 
 # GPIOの設定
 try:
@@ -51,12 +48,14 @@ except Exception as e:
     logging.error(f"GPIO設定エラー: {e}")
     exit(1)
 
+# JSTの設定
+jst = timezone(timedelta(hours=9))
+
 # プログラムの開始通知
 logging.info("通知：プログラム開始")
 
 def connect_to_aws():
     """AWS IoT Core への接続"""
-
     while True:
         try:
             myMQTTClient.connect()
@@ -68,23 +67,22 @@ def connect_to_aws():
 
 def publish_message():
     """AWSIoTCoreへメッセージを送信"""
-
-    # GPIOと時刻の初期状態の入力
+    # GPIOの初期状態の入力
     past_value = GPIO.input(gpio_pin)
 
     while True:
         value = GPIO.input(gpio_pin)
-        nowtime = datetime.now().replace(microsecond=0)
+        nowtime = datetime.now(jst).replace(microsecond=0)
 
         if value != past_value:
             if value == 1: #扉が開いたとき
                 msgjson = {
                     "timestamp": f"{nowtime}",
-                    "sensor_value": value
+                    "status": "open"
                 }
                 try:
                     myMQTTClient.publish(sensor_topic, json.dumps(msgjson), 1)
-                    logging.info(f"センサデータ送信成功通知：{msgjson}")
+                    logging.info(f"通知：センサデータ送信成功 topic:{sensor_topic}, message:{msgjson}")
                 except Exception as e:
                     logging.error(f"AWSパブリッシュエラー通知：{e}")
                     logging.info("通知：AWSとの再接続を試みます")
@@ -95,28 +93,26 @@ def publish_message():
 
 def send_heartbeat():
     """一定時間ごとに死活監視のメッセージを送信"""
-
     while True:
-        nowtime = datetime.now().replace(microsecond=0)
+        nowtime = datetime.now(jst).replace(microsecond=0)
         msgjson = {
             "timestamp": f"{nowtime}",
-            "heart_beat": 1
+            "status": "heartbeat"
         }
         try:
             myMQTTClient.publish(sensor_topic, json.dumps(msgjson), 1)
-            logging.info(f"ハートビート送信成功通知: {msgjson}")
+            logging.info(f"通知：ハートビート送信成功 topic:{sensor_topic}, message:{msgjson}")
         except Exception as e:
             logging.error(f"ハートビート送信失敗通知: {e}")
             logging.info("通知：AWSとの再接続を試みます")
             connect_to_aws()
         
-        time.sleep(interval)
+        time.sleep(publish_interval)
 
 def main():
     """メイン処理"""
     try:
         connect_to_aws()
-
         event_thread = threading.Thread(target=publish_message, daemon=True)
         heartbeat_thread = threading.Thread(target=send_heartbeat, daemon=True)
         event_thread.start()
